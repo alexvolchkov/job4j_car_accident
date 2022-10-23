f0decee904efb67699d1926efb1bf51dd64e2f1a
alexvolchkov <volkaa@mail.ru>
2022-10-23 22:22:22 +0300

    1. Представления [#504792]

--- db/schema.sql
+++ db/schema.sql
@@ -21,3 +21,14 @@ accident_id int not null references accident(id),
 rule_id int not null references rule(id),
 primary key(accident_id, rule_id)
 );
+
+create view show_all_accident_with_rule as
+select a.name accident_name,
+       a.text,
+       a.address,
+       at.name accident_type_name,
+       r.name rule_name
+from accident a
+         left join accident_type at on at.id = a.type_id
+join accident_rule ar on a.id = ar.accident_id
+join rule r on r.id = ar.rule_id;
